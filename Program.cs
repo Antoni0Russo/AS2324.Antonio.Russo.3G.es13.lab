bd2e493b86a029baff80aa92e3a40205fa9a2e74
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-02-22 09:09:49 +0100

    2 feat

--- Program.cs
+++ Program.cs
@@ -1,22 +1,70 @@
-﻿namespace AS2324.Antonio.Russo._3G.es13.lab
+﻿using System;
+
+namespace AS2324.Antonio.Russo._3G.es13.lab
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("quante persone voi inserire ");
-            int dimensione  = Convert.ToInt32(Console.ReadLine());
-            int[] peso = new int[dimensione];
-            int[] eta  = new int[dimensione];
-            Console.WriteLine("riempimento");
-            for (int i = 0; i < peso.Length ; i++)
-            {
-                Console.WriteLine($"peso persona { i + 1}");
-                peso[i] = Convert.ToInt32(Console.ReadLine());
+            double media = 0;
+            double min = 0;
+            double max = 0;
 
-                Console.WriteLine($"eta persona {i + 1}");
-                eta[i] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("quante persone voi inserire ");
+            int dimensione = Convert.ToInt32(Console.ReadLine());
+            int[] peso = new int[dimensione];
+            int[] eta = new int[dimensione];
+            Console.WriteLine("riempimento");
+
+            CaricaVettori(ref peso, ref eta);
+            Statistiche(ref peso, ref media, ref min, ref max, dimensione);
+            Console.WriteLine($" peso:  {peso}");
+            Console.WriteLine($" media:  {media}");
+            Console.WriteLine($" minimo:  {min}");
+            Console.WriteLine($" massimo:  {max}");
+        }
+        static void CaricaVettori(ref int[] peso,ref int[] eta)
+        {
+            for (int i = 0; i < peso.Length; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"peso persona {i + 1} tra 50 Kg a 100kg");
+                    peso[i] = Convert.ToInt32(Console.ReadLine());
+                } while (peso[i] < 50 || peso[i] > 100);
+
+
+                do
+                {
+                    Console.WriteLine($"eta persona {i + 1} tra 18 a 99");
+                    eta[i] = Convert.ToInt32(Console.ReadLine());
+                } while (eta[i] < 18 || eta[i] > 99);
+            }
+        }
+        static void Statistiche(ref int[] peso, ref double media,ref double min,ref double max,int dimensione )
+        {
+            int somma = 0;
+            for(int i = 0;i < peso.Length;i++)
+            {
+                somma = peso[i];
+            }
+            media = somma / dimensione;
+
+            max = peso[0];
+            for (int i = 1; i < peso.Length; i++)
+            {
+                if (peso[i] > max)
+                {
+                    max = peso[i];
+                }
+            }
+            min = peso[0];
+            for (int i = 1; i < peso.Length; i++)
+            {
+                if (peso[i] < min)
+                {
+                    min = peso[i];
+                }
             }
         }
     }
