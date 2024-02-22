95e7529b8b15993d241a7ae29857764086bbf938
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-02-22 08:46:53 +0100

    iniziallizzazione del programma

--- Program.cs
+++ Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("quante persone voi inserire ");
             int dimensione  = Convert.ToInt32(Console.ReadLine());
+            int[] peso = new int[dimensione];
+            int[] eta  = new int[dimensione];
+            Console.WriteLine("riempimento");
+            for (int i = 0; i < peso.Length ; i++)
+            {
+                Console.WriteLine($"peso persona { i + 1}");
+                peso[i] = Convert.ToInt32(Console.ReadLine());
 
-            
+                Console.WriteLine($"eta persona {i + 1}");
+                eta[i] = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
