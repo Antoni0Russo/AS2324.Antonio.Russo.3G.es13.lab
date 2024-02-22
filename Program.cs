09258837f9047e7af261ccea56336ca04a37017f
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-02-22 08:41:48 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AS2324.Antonio.Russo._3G.es13.lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quante persone voi inserire ");
+            int dimensione  = Convert.ToInt32(Console.ReadLine());
+
+            
+        }
+    }
+}
